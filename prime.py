81211fce65af7a094163d10522fe67dee6c118eb
riyaj-ym <riyaj.ym@gmail.com>
2023-01-02 22:32:12 +0530

    Added function for prime number

--- prime.py
+++ prime.py
@@ -1,13 +1,22 @@
 """Author:RYM
-Developer:RYM
+Developer:GRM
 Date: 02-01-2023
 Day: Monday"""
 
 number = int(input("Enter Number:\n"))
 
-for i in range(2, number):
-    if number % i == 0:
-        print(f"{number} is Not prime")
-        break
-else:
+prime = True
+
+
+def primeNumber(n):
+    primeN = True
+    for i in range(2, number):
+        if n % i == 0:
+            primeN = False
+    return primeN
+
+
+if primeNumber(number):
     print(f"{number} is prime")
+else:
+    print(f"{number} is not prime")
